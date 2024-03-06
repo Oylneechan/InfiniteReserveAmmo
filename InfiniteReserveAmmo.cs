a5eb6aa08f61a4904fedae9780e921f0a158ab36
Oylsister <naphatpromp@gmail.com>
2024-03-06 07:48:14 +0700

    Initial Commit

---
+++ InfiniteReserveAmmo.cs
@@ -0,0 +1,65 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+
+namespace InfiniteReserveAmmo
+{
+    public class InfiniteReserveAmmo : BasePlugin
+    {
+        public override string ModuleName => "Infinite Reserve Ammo";
+        public override string ModuleVersion => "1.0";
+        public override string ModuleAuthor => "Oylsister";
+
+        public override void Load(bool hotReload)
+        {
+            RegisterEventHandler<EventWeaponFire>(OnWeaponFire, HookMode.Post);
+        }
+
+        private HookResult OnWeaponFire(EventWeaponFire @event, GameEventInfo info)
+        {
+            var client = @event.Userid;
+            if (client != null && client.IsValid)
+            {
+                SetInfiniteBullet(client);
+            }
+
+            return HookResult.Continue;
+        }
+
+        void SetInfiniteBullet(CCSPlayerController client)
+        {
+            if (client.PawnIsAlive && client.LifeState == (byte)LifeState_t.LIFE_ALIVE)
+            {
+                var weapon = client.PlayerPawn.Value!.WeaponServices!.ActiveWeapon;
+                var weaponbase = weapon.Value!.As<CCSWeaponBase>();
+                if (weapon != null && (weaponbase.VData!.GearSlot == gear_slot_t.GEAR_SLOT_RIFLE || weaponbase.VData!.GearSlot == gear_slot_t.GEAR_SLOT_PISTOL))
+                {
+                    SetReserveAmmo(client, GetReserveAmmo(client) + 1);
+                }
+            }
+        }
+
+        int GetReserveAmmo(CCSPlayerController client)
+        {
+            var weapon = client.PlayerPawn.Value!.WeaponServices!.ActiveWeapon;
+
+            if (weapon == null || !weapon.IsValid)
+                return -1;
+
+            var ammo = weapon.Value!.ReserveAmmo[0];
+
+            return ammo;
+        }
+
+        void SetReserveAmmo(CCSPlayerController client, int ammo)
+        {
+            var weapon = client.PlayerPawn.Value!.WeaponServices!.ActiveWeapon;
+
+            if (weapon == null || !weapon.IsValid)
+                return;
+
+            weapon.Value!.ReserveAmmo[0] = ammo;
+
+            Utilities.SetStateChanged(weapon.Value.As<CCSWeaponBase>(), "CBasePlayerWeapon", "m_pReserveAmmo");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# InfiniteReserveAmmo
+ Set Infinite Reserve Ammo for Primary and Secondary weapon.
